43403309402fe8c076ae1fe6778e5de087062ba6
martvelasquez <113405472+martvelasquez@users.noreply.github.com>
2022-09-27 21:35:07 -0500

    clase11

---
+++ Class11/test.js
@@ -0,0 +1,10 @@
+let ejm1 = "y"
+let ejm = "x"
+
+//let ejm1 = 5
+//let ejm = 10
+//const x = 4 (esto es una forma de declarar variable)
+console.log(ejm1 * ejm)
+console.log(ejm1 + ejm)
+console.log(ejm1-ejm)
+console.log(ejm1/ejm)
