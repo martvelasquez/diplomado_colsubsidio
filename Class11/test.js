53495d29365c2d181e4e85ca08483f828553e329
martvelasquez <113405472+martvelasquez@users.noreply.github.com>
2022-09-27 22:12:48 -0500

    Actup

--- Class11/test.js
+++ Class11/test.js
@@ -1,10 +1,27 @@
-let ejm1 = "y"
-let ejm = "x"
+//let ejm1 = "y"
+//let ejm = "x"
 
 //let ejm1 = 5
 //let ejm = 10
 //const x = 4 (esto es una forma de declarar variable)
-console.log(ejm1 * ejm)
-console.log(ejm1 + ejm)
-console.log(ejm1-ejm)
-console.log(ejm1/ejm)
+//console.log(ejm1 * ejm)
+//console.log(ejm1 + ejm)
+//console.log(ejm1-ejm)
+//console.log(ejm1/ejm)
+
+/*let elemt1 = {
+    "valor": 5,
+    "nombre": "churro"
+}
+let elemt2 = {
+    "valor": 5,
+    "nombre": "churro"
+}
+console.log(elemt1.valor + elemt2.valor)
+console.log(elemt2.nombre + elemt1.nombre)
+console.log(elemt2.nombre + elemt2)*/
+
+const elem = "HOLA&MUNDO"
+for (const letter in elem) {
+    console.log(`${elem[letter]}`)
+}
